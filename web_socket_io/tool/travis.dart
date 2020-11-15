83c5b5f9e1cf5c8a3b84beb7fd5f6aef4569d925
alex <alex@tekartik.com>
2020-11-15 10:34:59 +0100

    fix: publish to none

--- web_socket_io/tool/travis.dart
+++ web_socket_io/tool/travis.dart
@@ -1,15 +1,5 @@
-import 'package:process_run/shell.dart';
+import 'package:dev_test/package.dart';
 
 Future main() async {
-  var shell = Shell();
-
-  await shell.run('''
-# Analyze code
-dartanalyzer --fatal-warnings --fatal-infos .
-dartfmt -n --set-exit-if-changed .
-
-# Run tests
-pub run test -p vm
-pub run build_runner test -- -p vm
-''');
+  await ioPackageRunCi('.');
 }
