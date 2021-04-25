ee31f1bdd2d48fed2a7915eb23d0a87baad8adf4
alex <alex@tekartik.com>
2021-04-25 07:56:58 +0200

    fix lint

--- web_socket_io/test/web_socket_io_test.dart
+++ web_socket_io/test/web_socket_io_test.dart
@@ -1,10 +1,9 @@
 @TestOn('vm')
 library tekartik_web_socket_io.test.web_socket_io_test;
 
-import 'package:test/test.dart';
 import 'package:tekartik_web_socket_io/web_socket_io.dart';
-
 import 'package:tekartik_web_socket_test/web_socket_test.dart';
+import 'package:test/test.dart';
 //import 'package:tekartik_serial_wss_client/channel/channel.dart';
 
 void main() {
