58a115fe560350912069d668dba480f9acd05112
Alexandre Roux <alex@tekartik.com>
2024-09-30 00:51:14 +0200

    fix lints

--- web_socket_io/test/web_socket_io_test.dart
+++ web_socket_io/test/web_socket_io_test.dart
@@ -1,5 +1,5 @@
 @TestOn('vm')
-library tekartik_web_socket_io.test.web_socket_io_test;
+library;
 
 import 'package:tekartik_web_socket_io/web_socket_io.dart';
 import 'package:tekartik_web_socket_test/web_socket_test.dart';
