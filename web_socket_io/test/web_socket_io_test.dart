03ac0878a3e9ba071598fa69ea9eb294d1e4ead6
Alexandre Roux <alex@tekartik.com>
2026-06-02 19:40:44 +0200

    dart 3.13 format

--- web_socket_io/test/web_socket_io_test.dart
+++ web_socket_io/test/web_socket_io_test.dart
@@ -4,6 +4,7 @@ library;
 import 'package:tekartik_web_socket_io/web_socket_io.dart';
 import 'package:tekartik_web_socket_test/web_socket_test.dart';
 import 'package:test/test.dart';
+
 //import 'package:tekartik_serial_wss_client/channel/channel.dart';
 
 void main() {
